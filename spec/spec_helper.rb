79fde35cecff603f44f74cb5a2c4509fe294768e
Brooks Swinnerton <bswinnerton@gmail.com>
2017-08-21 17:46:11 -0400

    Remove stub
    Since it's already in #1 to make for an easier diff.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,8 +8,6 @@ require 'pry'
 def app
   Faraday.new do |builder|
     builder.use Resilient::FaradayMiddleware
-    builder.adapter :test do |stub|
-      stub.get('/ok') { |env| [ 200, {}, 'OK' ] }
-    end
+    builder.adapter :test
   end
 end
