194572b22a6767348bc039c0eb4c711be26e5a5b
Brooks Swinnerton <bswinnerton@gmail.com>
2017-08-21 12:51:13 -0400

    Move app to method for easier re-use

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,12 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require 'faraday'
 require 'pry'
 require 'faraday_resilient'
+
+def app
+  Faraday.new do |builder|
+    builder.use FaradayResilient::Middleware
+    builder.adapter :test do |stub|
+      stub.get('/ok') { |env| [ 200, {}, 'OK' ] }
+    end
+  end
+end
